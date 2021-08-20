e2890301dc5db1eced52a1d22cb4a05d8a30dbd2
DinilR <84171952+DinilR@users.noreply.github.com>
2021-08-20 12:00:09 +0530

    Update Q2.scala

--- Assignment - Functions and Data/Q2.scala
+++ Assignment - Functions and Data/Q2.scala
@@ -1,10 +1,10 @@
 object Q2{
     def main(args:Array[String]):Unit = {
-        val a = new Rational(3, 4)
-        val b = new Rational(5, 8)
-        val c = new Rational(2, 7)
+        val x = new Rational(3, 4)
+        val y = new Rational(5, 8)
+        val z = new Rational(2, 7)
 
-        println(a.sub(b).sub(c))
+        println(x.sub(y).sub(zcd ))
     }
 }
 
